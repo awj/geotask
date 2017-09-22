96554d24d45b270f7f5122ddf8e09d7b578971bd
Adam Jones <ajones@Nadias-iMac.local>
2017-09-21 20:15:26 -0600

    Sett up json 404 rendering before we get any further.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,15 @@ class ApplicationController < ActionController::Base
   end
 
   def show_404
-    render file: "public/404.html", status: :not_found, layout: false
+    respond_to do |format|
+      format.html do
+        render file: "public/404.html", status: :not_found, layout: false
+      end
+
+      format.json do
+        render json: { "base" => [{ "error" => "not_found" }] },
+               status: :not_found
+      end
+    end
   end
 end
