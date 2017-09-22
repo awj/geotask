330dae6fa016da6d6e2619f81e0ef58b6de9603e
Adam Jones <ajones@Nadias-iMac.local>
2017-09-21 21:38:30 -0600

    Add some sample data to get things started.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p = Project.new name: "Park cleanup",
+                description: "Miscellaneous cleanup and maintenance of the park",
+                north: 39.908133,
+                south: 39.904536,
+                east:  -105.059492,
+                west:  -105.063291
+
+p.save!
+
+p.tasks.create!  name: "Remove graffiti",
+                 lat: 39.90522,
+                 lon: -105.059987,
+                 complete: false
+
+p.tasks.create!  name: "Mow grass",
+                 complete: false
+
+p.tasks.create!  name: "Replace lights",
+                 lat: 39.90792,
+                 lon: -105.06021,
+                 complete: false
+  
